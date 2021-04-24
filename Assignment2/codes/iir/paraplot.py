320df4974fcbf2554b430adb1dd7c3d2d4bff4e9
saimehar <ee18btech11029@iith.ac.in>
2021-04-24 14:54:11 +0530

    initialcommit

--- Assignment2/codes/iir/paraplot.py
+++ Assignment2/codes/iir/paraplot.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = 4
-for epsilon in np.arange(0.35,0.6,0.05): #0.35:0.05:0.6
+for epsilon in np.arange(0.35,0.6,0.05): 
     Omega1 = np.arange(0,1,0.01)
     H1 = 1/np.sqrt(1 + epsilon**2*np.cos(N*np.arccos(Omega1))**2)
-    Omega2 = np.arange(1,3.01,0.01) # 0:0.01:2
+    Omega2 = np.arange(1,3.01,0.01) 
     H2 = 1/np.sqrt(1 + epsilon**2*np.cosh(N*np.arccosh(Omega2))**2)
     H = np.concatenate((H1,H2))
     Omega = np.concatenate((Omega1,Omega2))
